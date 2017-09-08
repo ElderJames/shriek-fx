64969cf30c1c54f4ed64c2094b8c8df3e4b77124
ElderJames <shunjiey@hotmail.com>
2017-09-08 20:12:24 +0800

    重写asp.net core的控制器判断方法，使其自动把代理接口的实现作为控制器。

---
+++ src/Shriek.Mvc/Internal/ServiceControllerFeatureProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace Shriek.Mvc.Internal
+{
+    public class ServiceControllerFeatureProvider : ControllerFeatureProvider
+    {
+        private IEnumerable<Type> ServiceTypes;
+
+        public ServiceControllerFeatureProvider(IEnumerable<Type> ServiceTypes)
+        {
+            this.ServiceTypes = ServiceTypes;
+        }
+
+        protected override bool IsController(TypeInfo typeInfo)
+        {
+            var isController = base.IsController(typeInfo)
+                || ServiceTypes.Any(o => typeInfo.GetInterface(o.Name) == o);
+
+            return isController;
+        }
+    }
+}
--- src/Shriek.Mvc/ShriekMvcExtensions.cs
+++ src/Shriek.Mvc/ShriekMvcExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.DependencyInjection;
 using Shriek.Mvc.Internal;
 using Shriek.WebApi.Proxy.AspectCore;
@@ -13,12 +17,14 @@ namespace Shriek.Mvc
             var option = new WebApiProxyOptions();
 
             optionAction(option);
+            IEnumerable<Type> interfaceTypes = new List<Type>();
 
-            foreach (var o in option.WebApiProxies)
+            foreach (var proxy in option.WebApiProxies)
             {
-                var types = o.GetType().Assembly.GetTypes().Where(x =>
-                    x.IsInterface && x.GetMethods().SelectMany(m => m.GetCustomAttributes(typeof(ApiActionAttribute), true)).Any());
-                foreach (var t in types)
+                interfaceTypes = proxy.GetType().Assembly.GetTypes().Where(x =>
+                   x.IsInterface && x.GetMethods().SelectMany(m => m.GetCustomAttributes(typeof(ApiActionAttribute), true)).Any());
+
+                foreach (var t in interfaceTypes)
                 {
                     mvcBuilder.AddMvcOptions(opt =>
                     {
@@ -29,7 +35,43 @@ namespace Shriek.Mvc
                 }
             }
 
-            return mvcBuilder;
+            return mvcBuilder.ConfigureApplicationPartManager(manager =>
+            {
+                var featureProvider = new ServiceControllerFeatureProvider(interfaceTypes);
+                manager.FeatureProviders.Add(featureProvider);
+                mvcBuilder.Services.AddSingleton<IApplicationFeatureProvider<ControllerFeature>>(featureProvider);
+            }); ;
+        }
+
+        public static IMvcCoreBuilder UseWebApiProxy(this IMvcCoreBuilder mvcBuilder, Action<WebApiProxyOptions> optionAction)
+        {
+            var option = new WebApiProxyOptions();
+
+            optionAction(option);
+            IEnumerable<Type> interfaceTypes = new List<Type>();
+
+            foreach (var proxy in option.WebApiProxies)
+            {
+                interfaceTypes = proxy.GetType().Assembly.GetTypes().Where(x =>
+                    x.IsInterface && x.GetMethods().SelectMany(m => m.GetCustomAttributes(typeof(ApiActionAttribute), true)).Any());
+
+                foreach (var t in interfaceTypes)
+                {
+                    mvcBuilder.AddMvcOptions(opt =>
+                    {
+                        opt.Conventions.Add(new ControllerModelConvention(t));
+                        opt.Conventions.Add(new ActionModelConvention(t));
+                        opt.Conventions.Add(new ParameterModelConvention(t));
+                    });
+                }
+            }
+
+            return mvcBuilder.ConfigureApplicationPartManager(manager =>
+            {
+                var featureProvider = new ServiceControllerFeatureProvider(interfaceTypes);
+                manager.FeatureProviders.Add(featureProvider);
+                mvcBuilder.Services.AddSingleton<IApplicationFeatureProvider<ControllerFeature>>(featureProvider);
+            }); ;
         }
     }
 }
