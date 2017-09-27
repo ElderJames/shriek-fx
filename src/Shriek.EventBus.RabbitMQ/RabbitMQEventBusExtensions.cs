eff76273c1032fb1b8fde0f92441884ffaeb4553
ElderJames <shunjiey@hotmail.com>
2017-09-28 00:29:52 +0800

    添加RabbitMQ实现

---
+++ src/Shriek.EventBus.RabbitMQ/RabbitMQEventBusExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shriek;
+using Shriek.Messages;
+
+namespace Shriek.EventBus.RabbitMQ
+{
+    public static class RabbitMQEventBusExtensions
+    {
+        public static IShriekBuilder AddRabbitMqEventBus(this IShriekBuilder builder, Action<ConnectionFactory> optionAction)
+        {
+            var factory = new ConnectionFactory();
+
+            optionAction(factory);
+
+            //创建连接
+            var connection = factory.CreateConnection();
+            //创建通道
+            var channel = connection.CreateModel();
+
+            builder.Services.AddScoped(x => channel);
+
+            //声明一个队列
+            channel.QueueDeclare("eventQueue", false, false, false, null);
+
+            builder.Services.AddSingleton<IMessagePublisher, RabbitMQMessageProducer>();
+
+            return builder;
+        }
+    }
+}
---
+++ src/Shriek.EventBus.RabbitMQ/RabbitMQMessageProducer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shriek.Messages;
+
+namespace Shriek.EventBus.RabbitMQ
+{
+    public class RabbitMQMessageProducer : IMessagePublisher
+    {
+        private IModel channel;
+        private IServiceProvider container;
+
+        public RabbitMQMessageProducer(IModel channel, IServiceProvider container)
+        {
+            this.channel = channel;
+
+            //事件基本消费者
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+            //接收到消息事件
+            consumer.Received += (ch, ea) =>
+            {
+                var json = Encoding.UTF8.GetString(ea.Body);
+                var msg = JsonConvert.DeserializeObject<Message>(json);
+                var message = JsonConvert.DeserializeObject(json, Type.GetType(msg.MessageType));
+
+                var subscribers = container.GetServices(typeof(IMessageSubscriber<>).MakeGenericType(message.GetType()));
+
+                foreach (var sub in subscribers)
+                {
+                    ((dynamic)sub).Execute((dynamic)message);
+                }
+
+                //确认该消息已被消费
+                channel.BasicAck(ea.DeliveryTag, false);
+                //
+            };
+
+            //启动消费者 设置为手动应答消息
+            channel.BasicConsume("eventQueue", false, consumer);
+        }
+
+        public void Dispose()
+        {
+            channel.Dispose();
+        }
+
+        public void Send<TMessage>(TMessage message) where TMessage : Message
+        {
+            var msg = JsonConvert.SerializeObject(message);
+            var sendBytes = Encoding.UTF8.GetBytes(msg);
+
+            channel.BasicPublish("", "eventQueue", null, sendBytes);
+        }
+    }
+}
