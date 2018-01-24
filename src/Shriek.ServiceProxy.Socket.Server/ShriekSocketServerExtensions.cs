fd0c300c84c4930775f11ad00e9ddd7802e0f79b
ElderJames <shunjiey@hotmail.com>
2018-01-24 21:12:33 +0800

    feat(socket): 增加服务端扩展

---
+++ src/Shriek.ServiceProxy.Socket.Server/ShriekSocketServerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Shriek.ServiceProxy.Abstractions;
+
+namespace Shriek.ServiceProxy.Socket.Server
+{
+    public static class ShriekSocketServerExtensions
+    {
+        public static IShriekBuilder AddSocketServer(this IShriekBuilder builder, Action<WebApiProxyOptions> optionAction)
+        {
+            AppDomain.CurrentDomain.UpdateExcutingAssemblies();
+
+            var options = new WebApiProxyOptions();
+            optionAction(options);
+
+            foreach (var t in options.RegisteredServices.Select(x => x.Value))
+            {
+            }
+
+            return builder;
+        }
+    }
+}
