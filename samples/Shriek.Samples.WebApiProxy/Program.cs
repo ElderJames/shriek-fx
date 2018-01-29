cb19d958417d20ccee50b663d9a23fe223d23766
ElderJames <shunjiey@hotmail.com>
2018-01-29 16:08:28 +0800

    feat(samples): 增加socket通信的示例

--- samples/Shriek.Samples.WebApiProxy/Program.cs
+++ samples/Shriek.Samples.WebApiProxy/Program.cs
@@ -3,6 +3,12 @@ using Shriek.Samples.WebApiProxy.Contracts;
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using Shriek.Samples.WebApiProxy.Models;
+using Shriek.ServiceProxy.Http;
+using Shriek.ServiceProxy.Http.Server;
 using Shriek.ServiceProxy.Socket;
 using Shriek.ServiceProxy.Socket.Core;
 using Shriek.ServiceProxy.Socket.Fast;
@@ -14,131 +20,99 @@ namespace Shriek.Samples.WebApiProxy
     {
         public static void Main(string[] args)
         {
-            //var listener = new TcpListener();
-            //listener.Use<FastMiddleware>();
-            //listener.Start(1212);
+            new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:8080", "http://*:8081")
+                .ConfigureServices(services =>
+                {
+                    //tcp服务端
+                    services.AddSocketServer(option =>
+                    {
+                        option.Port = 1212;
+                        option.AddService<ISimpleInterface>();
+                    });
 
-            var services = new ServiceCollection().AddShriek().AddSocketServer(option =>
-            {
-                option.Port = 1212;
-                option.AddService<ISimpleInterface>();
-            });
+                    //tcp客户端
+                    services.AddSocketProxy(options =>
+                    {
+                        options.Port = 1212;
+                        options.AddService<ISimpleInterface>();
+                    });
+
+                    services.AddMvcCore()
+                        .AddJsonFormatters()
+                        .AddWebApiProxyServer(opt =>
+                            {
+                                opt.AddWebApiProxy<SampleApiProxy>();
+                                opt.AddWebApiProxy<Samples.Services.SampleApiProxy>();
+                                opt.AddService<Samples.Services.ITestService>();
+                            });
+
+                    //服务里注册代理客户端
+                    services.AddWebApiProxy(opt => { opt.AddWebApiProxy<SampleApiProxy>("http://localhost:8081"); });
+                })
+                .Configure(app =>
+                {
+                    app.Use(async (context, next) =>
+                    {
+                        try
+                        {
+                            await next();
+                        }
+                        catch (Exception ex)
+                        {
+                            throw ex;
+                        }
+                    });
+                    app.UseMvc();
+                })
+                .Build()
+                .Start();
+
+            var provider = new ServiceCollection()
+                .AddWebApiProxy(opt =>
+                {
+                    opt.AddWebApiProxy<SampleApiProxy>("http://localhost:8081");
+                    opt.AddWebApiProxy<Samples.Services.SampleApiProxy>("http://localhost:8080");
+                    opt.AddService<Samples.Services.ITestService>("http://localhost:8080");
+                })
+                .AddSocketProxy(options =>
+                {
+                    options.Port = 1212;
+                    options.AddService<ISimpleInterface>();
+                })
+                .BuildServiceProvider();
+
+            var todoService = provider.GetService<ITodoService>();
+            var testService = provider.GetService<ITestService>();
+            var sampleTestService = provider.GetService<Samples.Services.ITestService>();
+            var tcpService = provider.GetService<ISimpleInterface>();
 
             Console.ReadKey();
 
-            var provider = new ServiceCollection().AddSocketProxy(options =>
-            {
-                options.Port = 1212;
-                options.AddService<ISimpleInterface>();
-            }).BuildServiceProvider();
+            var createRsult = todoService.Create(new Todo() { Name = "james" }).Result;
+            Console.WriteLine(JsonConvert.SerializeObject(createRsult));
 
-            var simpleInterfaceService = provider.GetService<ISimpleInterface>();
-            var users = simpleInterfaceService.Test("elderjames").Result;
+            var result = todoService.Get(1).Result;
+            Console.WriteLine(JsonConvert.SerializeObject(result));
 
-            //var client = new FastTcpClient();
-            //client.Connect(IPAddress.Loopback, 1212);
-            //var users = client.InvokeApi<string>("Test", "elderjames").GetResult();
-            Console.Write(users);
+            result = todoService.Get(2).Result;
+            Console.WriteLine(JsonConvert.SerializeObject(result));
+
+            var typeResult = todoService.GetTypes(new[] { Contracts.Type.起床, Contracts.Type.睡觉 }, "james", 10);
+            Console.WriteLine(JsonConvert.SerializeObject(typeResult));
+
+            //这个调用服务，服务内注入了一个代理客户端调用另一个服务
+            var result2 = testService.Test(11);
+            Console.WriteLine(JsonConvert.SerializeObject(result2));
+
+            var result3 = sampleTestService.Test("elderjames").Result;
+            Console.WriteLine(JsonConvert.SerializeObject(result3));
+
+            var result4 = tcpService.Test("hahaha").Result;
+            Console.WriteLine(JsonConvert.SerializeObject(result4));
 
             Console.ReadKey();
-            //    new WebHostBuilder()
-            //        .UseKestrel()
-            //        .UseUrls("http://*:8080", "http://*:8081")
-            //        .ConfigureServices(services =>
-            //        {
-            //            services.AddMvcCore()
-            //                .AddJsonFormatters()
-            //                .AddWebApiProxyServer(opt =>
-            //                    {
-            //                        opt.AddWebApiProxy<SampleApiProxy>();
-            //                        opt.AddWebApiProxy<Samples.Services.SampleApiProxy>();
-            //                        opt.AddService<ISimpleInterface>();
-            //                    });
-
-            //            //服务里注册代理客户端
-            //            services.AddWebApiProxy(opt =>
-            //            {
-            //                opt.AddWebApiProxy<SampleApiProxy>("http://localhost:8081");
-            //            });
-            //        })
-            //        .Configure(app =>
-            //        {
-            //            app.Use(async (context, next) =>
-            //            {
-            //                try
-            //                {
-            //                    await next();
-            //                }
-            //                catch (Exception ex)
-            //                {
-            //                    throw;
-            //                }
-            //            });
-            //            app.UseMvc();
-            //        })
-            //        .Build()
-            //        .Start();
-
-            //    var provider = new ServiceCollection()
-            //        .AddWebApiProxy(opt =>
-            //        {
-            //            opt.AddWebApiProxy<SampleApiProxy>("http://localhost:8081");
-            //            opt.AddWebApiProxy<Samples.Services.SampleApiProxy>("http://localhost:8080");
-            //            opt.AddService<ISimpleInterface>("http://localhost:8080");
-            //        })
-            //        .BuildServiceProvider();
-
-            //    var todoService = provider.GetService<ITodoService>();
-            //    var testService = provider.GetService<ITestService>();
-            //    var sampleTestService = provider.GetService<Samples.Services.ITestService>();
-            //    var tcpService = provider.GetService<ISimpleInterface>();
-
-            //    Console.ReadKey();
-
-            //    var createRsult = todoService.Create(new Todo() { Name = "james" }).Result;
-            //    Console.WriteLine(JsonConvert.SerializeObject(createRsult));
-
-            //    var result = todoService.Get(1).Result;
-            //    Console.WriteLine(JsonConvert.SerializeObject(result));
-
-            //    result = todoService.Get(2).Result;
-            //    Console.WriteLine(JsonConvert.SerializeObject(result));
-
-            //    var typeResult = todoService.GetTypes(new[] { Contracts.Type.起床, Contracts.Type.睡觉 }, "james", 10);
-            //    Console.WriteLine(JsonConvert.SerializeObject(typeResult));
-
-            //    //这个调用服务，服务内注入了一个代理客户端调用另一个服务
-            //    var result2 = testService.Test(11);
-            //    Console.WriteLine(JsonConvert.SerializeObject(result2));
-
-            //    var result3 = sampleTestService.Test("elderjames").Result;
-            //    Console.WriteLine(JsonConvert.SerializeObject(result3));
-
-            //    var result4 = tcpService.Test("hahaha").Result;
-            //    Console.WriteLine(JsonConvert.SerializeObject(result4));
-
-            //    Console.ReadKey();
         }
-    }
-
-    public class FastService : FastApiService
-    {
-        [Api]
-        public UserInfo[] UserInfo(string name)
-        {
-            return new UserInfo[0];
-        }
-
-        [Api]
-        public async Task<string> AboutAsync(string name)
-        {
-            await Task.Delay(TimeSpan.FromSeconds(1));
-            return "Fast->" + name;
-        }
-    }
-
-    public class UserInfo
-    {
-        public string Name { get; set; }
     }
 }
