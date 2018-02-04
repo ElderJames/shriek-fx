6aa135bd8057b687440e4b407ada7051eb69bc4c
ElderJames <shunjiey@hotmail.com>
2018-02-05 00:56:39 +0800

    feat(msgpack): 增加序列化器msgpack

---
+++ src/Shriek.Serialization.MessagePack/MessagePackSerializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Shriek.Serialization.MessagePack
+{
+    public class MessagePackSerializer : ISerializer
+    {
+        public MessagePackSerializer()
+        {
+        }
+
+        public dynamic Deserialize(Type objType, string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Deserialize<T>(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Serialize(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic SerializeDynamic(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Shriek/Serialization/ISerializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shriek.Serialization
+{
+    public interface ISerializer
+    {
+        dynamic SerializeDynamic(object obj);
+
+        string Serialize(object obj);
+
+        dynamic Deserialize(Type objType, string str);
+
+        T Deserialize<T>(string str);
+    }
+}
