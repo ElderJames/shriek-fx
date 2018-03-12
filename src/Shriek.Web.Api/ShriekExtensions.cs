00c54ea2768545363091ef6b34b8ffea31c51291
ElderJames <shunjiey@hotmail.com>
2018-03-12 18:17:58 +0800

    feat(webapi): add Shriek.Web.Api project for webapi extensions

---
+++ src/Shriek.Web.Api/ShriekExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shriek.Web.Api
+{
+    public static class ShriekExtensions
+    {
+        public static IShriekBuilder AddScriptProxyGenerator(this IShriekBuilder builder)
+        {
+            return builder;
+        }
+    }
+}
