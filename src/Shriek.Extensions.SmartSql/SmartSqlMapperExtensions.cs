1e8aba683d30fb73153c1383b3640ef98aa85edc
ElderJames <shunjiey@hotmail.com>
2018-04-28 12:25:46 +0800

    feat(extra): 增加smartsql扩展，优化配置和使用方式

---
+++ src/Shriek.Extensions.SmartSql/SmartSqlMapperExtensions.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SmartSql.Abstractions;
+
+namespace Shriek.Extensions.SmartSql
+{
+	public static class SmartSqlMapperExtensions
+	{
+		#region async
+
+		public static Task<int> ExecuteAsync(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.ExecuteAsync(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			});
+		}
+
+		public static Task<T> ExecuteScalarAsync<T>(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.ExecuteScalarAsync<T>(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			});
+		}
+
+		public static Task<IEnumerable<T>> QueryAsync<T>(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.QueryAsync<T>(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			});
+		}
+
+		public static Task<IEnumerable<T>> QueryAsync<T>(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params, DataSourceChoice sourceChoice)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.QueryAsync<T>(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			}, sourceChoice);
+		}
+
+		public static Task<T> QuerySingleAsync<T>(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.QuerySingleAsync<T>(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			});
+		}
+
+		public static Task<T> QuerySingleAsync<T>(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params, DataSourceChoice sourceChoice)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.QuerySingleAsync<T>(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			}, sourceChoice);
+		}
+
+		#endregion async
+
+		#region sync
+
+		public static int Execute(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.Execute(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			});
+		}
+
+		public static T ExecuteScalar<T>(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.ExecuteScalar<T>(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			});
+		}
+
+		public static IEnumerable<T> Query<T>(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.Query<T>(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			});
+		}
+
+		public static IEnumerable<T> Query<T>(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params, DataSourceChoice sourceChoice)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.Query<T>(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			}, sourceChoice);
+		}
+
+		public static T QuerySingle<T>(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.QuerySingle<T>(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			});
+		}
+
+		public static T QuerySingle<T>(this ISmartSqlMapper sqlMapper, string fullSqlId, dynamic @params, DataSourceChoice sourceChoice)
+		{
+			EnsurePoint(ref fullSqlId);
+
+			return sqlMapper.QuerySingle<T>(new RequestContext()
+			{
+				Scope = fullSqlId.Split('.')[0],
+				SqlId = fullSqlId.Split('.')[1],
+				Request = @params
+			}, sourceChoice);
+		}
+
+		#endregion sync
+
+		private static void EnsurePoint(ref string fullSqlId)
+		{
+			if (!fullSqlId.Contains(".")) fullSqlId = "." + fullSqlId;
+		}
+	}
+}
---
+++ src/Shriek.Extensions.SmartSql/SmartSqlOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Common;
+using Microsoft.Extensions.Options;
+
+namespace Shriek.Extensions.SmartSql
+{
+	public class SmartSqlOptions : IOptions<SmartSqlOptions>
+	{
+		public SmartSqlOptions Value => this;
+
+		public string SqlMapperPath { get; set; }
+
+		public DbProviderFactory DbProviderFactory { get; set; }
+
+		public string ConnectionString { get; set; }
+
+		public string LoggingName { get; set; }
+
+		public string ParameterPrefix { get; set; } = "@";
+
+		public bool UseManifestResource { get; set; }
+	}
+}
