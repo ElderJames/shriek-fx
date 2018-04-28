984dc9638ad0a15686fd3fe989052ff143234d0f
James Yeung <shunjiey@hotmail.com>
2018-04-28 12:37:05 +0800

    添加SmartSql扩展 (#11)
    * feat(dotnetty): 添加反射类
    
    * feat(extra): 增加smartsql扩展，优化配置和使用方式
    
    * feat(): 更新readme

---
+++ src/Shriek.Extensions.SmartSql/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MapperContainer = SmartSql.MapperContainer;
+
+namespace Shriek.Extensions.SmartSql
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static void UseSmartSql(this IServiceCollection services, Action<SmartSqlOptions> optionAction)
+		{
+			var options = new SmartSqlOptions();
+			optionAction(options);
+
+			services.AddSingleton(sp =>
+			{
+				var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+				return MapperContainer.Instance.GetSqlMapper(loggerFactory, string.Empty, new NativeConfigLoader(loggerFactory, options));
+			});
+		}
+	}
+}
