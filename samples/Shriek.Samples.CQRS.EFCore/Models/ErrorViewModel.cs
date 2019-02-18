e68ae37524df3c459b0eb724a1e3625afb7d6851
ElderJames <shunjiey@hotmail.com>
2019-02-18 12:28:42 +0800

    delete useless folder

--- samples/Shriek.Samples.CQRS.EFCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Shriek.Samples.CQRS.EFCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
